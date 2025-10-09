61f6475f4bf3133a9fa50e7dfa9158a34d1a4840
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-09 07:06:09 -0400

    2-dars python.py

---
+++ 2-dars python.py
@@ -0,0 +1,18 @@
+# print funksiyasi consolega chiqarish uchun ishlatiladi
+print("Hello, World!")
+print("Salom, Dunyo!")
+print(5)
+print(10 + 3)
+print(7 * 3)
+print(10 / 2)
+print(10 // 3)  # Butun sonlarni bo‘lish
+print(10 % 3)   # Qoldiqni chiqarish
+print(2 ** 3)  # Darajaga ko‘tarish
+print(3 + 5 * 2)  # Amallar tartibi
+print((3 + 5) * 2)  # Qavs ichidagi amal birinchi bajariladi
+print("3 + 5 =", 3 + 5)  # Matn va natijani birga chiqarish
+print("3 * 5 =", 3 * 5)
+print("10 / 3 =", 10 / 3)
+print("10 // 3 =", 10 // 3)
+print("10 % 3 =", 10 % 3)
+print("2 ** 3 =", 2 ** 3)
