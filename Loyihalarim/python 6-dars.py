0f2d94e32f549b943fa5b06c042ea28bac9e86fa
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-23 15:39:19 +0500

    updated

---
+++ Loyihalarim/python 6-dars.py
@@ -0,0 +1,21 @@
+#Ro'yhatlar 
+mevalar=["olma","anor",'olxo\'ri']
+mashinalar=["spark", "BMW 👑", "Mers 🚮🚯"]
+#print(mevalar)
+#index
+print(mevalar[1])
+print(mashinalar[2])
+print("It's my love",mashinalar[1])
+print(mashinalar[1].upper())
+print(mashinalar[2].lower())
+"""    IGNORE ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !"""
+animals=["tiger","lion","monkey","donkey","bird"]
+animals.remove("monkey")
+animals.insert(2,"donkey")
+print(animals)
+""" IGNORE  """
+ITechniklar=[]
+ITechniklar.append("Boboxon Jumaboyev")
+ITechniklar.append("Ixtiyorbek Baxtiyorov")
+ITechniklar.append("Elbek bot")
+print(ITechniklar)
