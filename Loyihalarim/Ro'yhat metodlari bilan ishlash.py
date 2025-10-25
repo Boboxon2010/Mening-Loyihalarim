f96307114bcfbba3e2949dcc559233cda88ec4e7
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-25 16:15:14 +0500

    updated

---
+++ Loyihalarim/Ro'yhat metodlari bilan ishlash.py
@@ -0,0 +1,29 @@
+"""
+25.10.2025
+Ro'yxatlar bilan ishlash.Methodlar va function lar bilan ishlash
+methods(.sport())
+function(sum(),min(),max(),sport())
+ro'tlardan nusxa olish
+Tuple() O'zgarmas ro'yhatlar
+"""
+
+# davlatlar=["Uzbekiston","AQSH","Rossiya","Xitoy","Koreya"]
+# mashinalr=["BMW👑","Mers🚮","Kia","Chevrolet","Damas"]
+# print(davlatlar)
+# print(mashinalr)
+
+# mashinalar=["BMW👑","Mers🚮","Audi","Chevrolet🔟","Damas😂"]
+# mashinalar.sort()
+# print(mashinalar)
+
+
+
+
+sonlar=list((4,3,5,6,2,1,7,8,9,0))
+print(sonlar)
+
+sonlar.sort(reverse=True)
+print(sonlar)
+sonlar1=list(range(1,201,2))
+print(sonlar1)
+
