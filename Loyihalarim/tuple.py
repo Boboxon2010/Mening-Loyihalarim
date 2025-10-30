94835f9466b62a6334f36517318ce2d12c936e94
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-30 16:04:58 +0500

    updated

--- Loyihalarim/tuple.py
+++ Loyihalarim/tuple.py
@@ -1,3 +1,4 @@
+"""
 ismlar=["Ixtiyor","Elbek","Ahrorbek"] #oddiy ro'yhat
 familya=("Baxtiyorov","Jabborov","Komiljanov") #o'zgarmas ro'yhat
 print("oddiy ro'yhat",ismlar)
@@ -8,3 +9,32 @@ last_name=list(familya)
 last_name.append("Davletov")
 familya=tuple(last_name)
 print(familya)
+"""
+#amaliyot
+davlatlar=["O'zbekiston","Rossiya","Portugaliya","Fransiya","Qirg'iziston","Turkmaniston"]
+
+print(davlatlar)
+
+print(len(davlatlar))
+
+print(sorted(davlatlar))
+
+print(sorted(davlatlar,reverse=True))
+
+print(davlatlar)
+
+print(reversed(davlatlar))
+
+print(davlatlar.sort())
+
+print(davlatlar.sort(reverse=True))
+
+juft_sonlar=list(range(120,1201))
+
+print(range(120, 1201))
+
+print(max(juft_sonlar)-min(juft_sonlar))
+
+print(len(juft_sonlar))
+
+print("Juft sonlar ro'yhatidagi oldingi 20 ta son:",juft_sonlar[range(20)],"Juft sonlar ro'yhatidagi oxirgi 20 ta qiymat:",juft_sonlar[range(-20)])
