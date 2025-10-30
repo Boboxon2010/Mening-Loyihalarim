f3dfa5c93afd11ab83f7c0a609d055d272699cce
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-30 16:26:32 +0500

    updated

--- Loyihalarim/tuple.py
+++ Loyihalarim/tuple.py
@@ -13,28 +13,29 @@ print(familya)
 #amaliyot
 davlatlar=["O'zbekiston","Rossiya","Portugaliya","Fransiya","Qirg'iziston","Turkmaniston"]
 
+print("davlatlar ro'yhati asl nusxasi:",davlatlar)
+
+print("Davlatlar ro'yhatidagi elementlar soni:",len(davlatlar))
+
+print("davlatlar ro'yhatini alifbo bo'yicha tartibi:",sorted(davlatlar))
+
+print("davlatlar ro'yhatini teskari tartibi:",sorted(davlatlar,reverse=True))
+
 print(davlatlar)
 
-print(len(davlatlar))
-
-print(sorted(davlatlar))
-
-print(sorted(davlatlar,reverse=True))
-
-print(davlatlar)
-
-print(reversed(davlatlar))
-
-print(davlatlar.sort())
-
-print(davlatlar.sort(reverse=True))
+print("davlatlar ro'yhatini teskari tartibi",reversed(davlatlar))
+davlatlar.sort()
+print("davlatlar ro'yhatini alifbo bo'yicha tartibi:",davlatlar)
+davlatlar.sort(reverse=True)
+print("davlatlar ro'yhatini teskari tartibi:",davlatlar)
 
 juft_sonlar=list(range(120,1201,2))
 
-print(sum(juft_sonlar))
+print("juft sonlar ro'yhatidagi qiymatlar yig'indisi:",sum(juft_sonlar))
 
-print(max(juft_sonlar)-min(juft_sonlar))
+print("Juft sonlar ro'yhatidagi elementlarning eng katta va eng kichikining farqi:",max(juft_sonlar)-min(juft_sonlar))
 
-print(len(juft_sonlar))
+print("Juft sonlar ro'yhatidagi elementlar soni:",len(juft_sonlar))
 
 print("Juft sonlar ro'yhatidagi oldingi 20 ta son:",juft_sonlar[:20],"Juft sonlar ro'yhatidagi oxirgi 20 ta qiymat:",juft_sonlar[20:])
+
