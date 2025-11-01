6ad3691a6f5ef7473313e4c861de275e00468854
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-01 16:03:14 +0500

    updated

---
+++ Loyihalarim/for loop.py
@@ -0,0 +1,18 @@
+# oquvchilar=["Aslam", "Baxodir", "Boboxon", "Sardor", "Jahongir", "Sherzod", "Bekzod", "Alisher", "Farruk", "Rustam", "Islom", "Akbar", "Jamshid", "Bahrom", "Shavkat", "Ravshan", "Oybek", "Firuz", "Dilshod", "Nodir", "Temur", "Javohir", "Aziz", "Botir", "Davron", "Elyor", "Firdavs", "Ibrohim", "Kamol", "Lazzat", "Muhsin", "Naim", "Orif", "Po'lat", "Qodir", "Rahim", "Sobir", "To'lqin", "Ulug'bek", "Vohid", "Xurshid", "Yorqin", "Zafar", "Siroj", "Mansur", "Najib", "Komil", "Ilhom", "Halim", "Anvar"]
+# for i in oquvchilar:
+#     print(f"Salom {i}")
+#     print(f"Xayr {i}")
+""""""
+# raqamlar=[1,5,9,-8,63]
+# for k in raqamlar:
+#     print(k**2)
+""""""
+# for i in range(30,91):
+#     print(i/9)
+""""""
+daraja=[]
+for i in range(27,54,7):
+    a=i**4
+    daraja.append(a)
+print(daraja)
+    
