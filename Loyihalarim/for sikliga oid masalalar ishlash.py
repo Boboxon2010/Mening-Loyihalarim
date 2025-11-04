3291569503bb6cab88ab2343dffbacd31c84e4d7
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-04 16:26:03 +0500

    updated

--- Loyihalarim/for sikliga oid masalalar ishlash.py
+++ Loyihalarim/for sikliga oid masalalar ishlash.py
@@ -2,6 +2,15 @@
 # n=float(input())
 # for i in range(1,11):
 #     print(i,"kg konfet",i*n,"so'm turadi")
-
+# for 5
 import numpy as np
-print(np.__version__)
+
+n = float(input("1 kg konfet narxini kiriting (so'mda): "))
+
+# 0.1 dan 1.0 gacha 0.1 qadam bilan
+for i in np.arange(0.1, 1.1, 0.1):
+    print(f"{i} kg konfet narxi {i*n} so'm turadi")
+
+#for 6
+for i in np.arange(1.2,2.2,0.2):
+    print(f"{i} kg konfetning narxi {i*n} so'm turadi")
