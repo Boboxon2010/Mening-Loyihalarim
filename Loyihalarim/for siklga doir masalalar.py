cbc5140a3679a26883e5fdf1bf2e3d3ee2341786
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-06 16:13:13 +0500

    updated

---
+++ Loyihalarim/for siklga doir masalalar.py
@@ -0,0 +1,21 @@
+#for7
+# a,b=map(int,input().split())
+# yigindi=sum(range(a,b))
+# print(yigindi)
+
+#for 8
+# a,b=map(int,input().split())
+# list=list(range(a,b+1))
+# s=1
+# for i in list:
+#     s=s*i
+# print(s)
+
+# for 9
+a,b=map(int,input().split())
+list=list(range(a,b+1))
+s=0
+for i in list:
+    s=s+pow(i,2)
+print(s)
+
