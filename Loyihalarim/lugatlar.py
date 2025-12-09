aef47d3b09817c33df760175a968ce92499620a5
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-12-09 15:49:37 +0500

    updated

---
+++ Loyihalarim/lugatlar.py
@@ -0,0 +1,21 @@
+car0={
+    "model":"BMW",
+    "yili":"2026",
+    "probeg":"hali ihlab chiqarilmagan",
+    "narxi":"1 000 000 $"
+}
+lugat={
+    "apple":"olma",
+    "another":"boshqa bir",
+    "banana":"banan",
+    "brown":"jigar rang",
+    "back":"orqaga",
+    "backpack":"papka",
+    "burn":"yonmoq,kuymoq",
+    "crocodile":"timsoh",
+    "chocalate":"shokalad",
+    "elephant":"fil",
+    "eraser":"o'chrigich",
+    "easy":"oson",
+    
+}
