5f67866a037c2c96232ed3dbec7e0a12684ac93f
Boboxon <boboxonjumaboyev2010@gmail.com>
2026-02-03 17:20:20 +0500

    updated

---
+++ Loyihalarim/funksiyalar.py
@@ -0,0 +1,16 @@
+#funksiyalar
+
+# def salom_ber(ism):
+#     print("Assalomu aleykum, hurmatli ", ism.title())
+
+# def kopaytma(x,y):
+#     print(f" {x}ni {y}ga ko'paytmasi {x*y} ga teng")
+# kopaytma(3,4)
+
+# def orta_gem(x,y,z):
+#     print(pow(x*y*z,1/3))
+# orta_gem(5,6,7)
+
+# def orta_arf(x,y,z):
+#     print((x+y+z)/3)
+# orta_arf(2,4,6)
