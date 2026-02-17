b505d98ae796858c66464568d9951911b2501312
Boboxon <boboxonjumaboyev2010@gmail.com>
2026-02-17 16:48:13 +0500

    updated

--- Loyihalarim/funksiyalar 2.py
+++ Loyihalarim/funksiyalar 2.py
@@ -1,1 +1,8 @@
-#return  funksiydagi qiymat qaytaruvchi kalit so'z
+def InvertDigit(a, b, c):
+    a = int(str(a)[::-1])
+    b = int(str(b)[::-1])
+    c = int(str(c)[::-1])
+    return a, b, c
+
+sonlar = InvertDigit(68871, 54687685, 5644)
+print(sonlar)
